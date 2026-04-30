c7830accf562a627ee0a65b3dd34e47a8fb67a4c
sthefany.maria <sthefany1morais@gmail.com>
2026-04-30 14:50:01 -0300

    feat:Adição de SplashScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,21 @@ import { ThemeProvider } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
 import { useEffect } from "react";
 import { LightTheme, DarkTheme } from "@/theme/theme";
+import { useTaskStore } from "@/store/taskStore";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const loadTasks = useTaskStore((state) => state.loadTasks);
 
   useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
+    // ✅ Carrega tasks E esconde splash
+    loadTasks().finally(async () => {
+      await SplashScreen.hideAsync();
+    });
+  }, [loadTasks]);
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : LightTheme}>
